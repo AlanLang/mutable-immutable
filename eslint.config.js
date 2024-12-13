c530ed21dea2168c1c1cbd28c93d5356bb0ab57f
alan <langwdalan@gmail.com>
2024-12-13 15:32:02 +0800

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,7 +25,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      "indent": ["error", 2]
+      "indent": ["error", 2],
+      "react/jsx-max-props-per-line": ["error", { maximum: 2 }],
+      "react/jsx-first-prop-new-line": ["error", "multiline"],
+      "react/jsx-closing-bracket-location": ["error", "tag-aligned"]
     },
   },
 )
